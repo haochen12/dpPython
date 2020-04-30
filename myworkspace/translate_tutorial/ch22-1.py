e9395bb9f6e2455f2eab4113719dfaf77a734e46
aron <haochen12>
2020-04-30 22:08:01 +0800

    modify param npnpnp_word as num_words

--- myworkspace/translate_tutorial/ch22-1.py
+++ myworkspace/translate_tutorial/ch22-1.py
@@ -13,7 +13,7 @@ numpy.random.seed(seed)
 
 # load the dataset but only keep the top n words, zero the rest
 top_words = 5000
-(X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=top_words)
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
 
 max_words = 500
 X_train = sequence.pad_sequences(X_train, maxlen=max_words)
